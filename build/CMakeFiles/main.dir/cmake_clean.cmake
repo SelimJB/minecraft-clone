af4af1dfd9d477841429a43972fd63ffc8e6c8a2
Unknown <selim.bennani@gmail.com>
2019-08-13 14:08:59 +0200

    Skeleton

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main.cpp.obj"
+  "CMakeFiles/main.dir/src/glad.c.obj"
+  "main.pdb"
+  "main.exe"
+  "main.exe.manifest"
+  "libmain.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
